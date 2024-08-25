e792c4b6468ff78241a3f1a03a085d42bb9e59ae
Argha0004 <156917192+Argha0004@users.noreply.github.com>
2024-08-25 19:48:02 +0000

    lecture_3

---
+++ 01_basics/lecture_3.js
@@ -0,0 +1,27 @@
+/* Datatypes and ECMA standards */
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+// always must be doing code readability should high
+
+let name = "argha" // String Data type
+let age = 20 // Integer Data type
+let isLoggedIn = false // Boolean Data type
+let state;
+
+// PREMITIVE DATATYPE --->>>
+// Integer means number => 2^53
+// bigint for big name like stock market, trading
+// String => ""
+// Boolean => true/flase
+// null => standalone value
+// undefined => 
+// symbol => for unique
+
+
+// Object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null); // object
+
