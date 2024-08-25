b80a0942ca5492b7635cb72941f76f9612bd7b96
Argha0004 <156917192+Argha0004@users.noreply.github.com>
2024-08-25 19:19:42 +0000

    container config done

--- 01_basics/lecture_2.js
+++ 01_basics/lecture_2.js
@@ -1,1 +1,23 @@
-/*  */
+/* Veriable --->>> Const, Let, Var */
+const accountId = 12022002029007
+let accountEmail = "arghadalal354@gmail.com"
+var accountPassword = "4161" // not use var only use const, let
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "arghadalal82gmail.com"
+accountPassword = "argha@4253"
+accountCity = "Rampurhat"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
